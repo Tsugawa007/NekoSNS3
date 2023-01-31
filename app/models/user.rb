7dde5396a2f6054dc5b328b4bcc1b3de7fa175c3
Tsugawa007 <59519629+Tsugawa007@users.noreply.github.com>
2023-01-31 12:52:20 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :name,{presence: true,uniqueness: true}
-    validates :email,{presence: true,uniqueness: true}
+    #validates :email,{presence: true,uniqueness: true}
 
     def posts
         return Post.where(user_id: self.id)
