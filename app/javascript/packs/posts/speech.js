5778f88a9d95305d4b998356e19bd00eedca7b41
Tsugawa007 <59519629+Tsugawa007@users.noreply.github.com>
2023-01-31 12:51:46 +0900

    Update speech.js

--- app/javascript/packs/posts/speech.js
+++ app/javascript/packs/posts/speech.js
@@ -12,46 +12,47 @@ catName.continious = false;
 catName.interiumResults = false;
 catName.lang = 'ja-JP';
 var text = "";
+var countTap = 0;
+var btn = document.getElementById('cat-name-button');
+var content = document.getElementById('cat-name-content');
 
 window.addEventListener('load',function(){
-    //setting  nums of tapping
-    var countTap = 0;
-    //setting Button 
-    var btn = document.getElementById('cat-button');
-    var content = document.getElementById('cat-name-content');
-    if(!btn || !content){
+  if(!btn || !content){
         return false;
+    }   
+},false);
+
+btn.addEventListener('click',function(){
+
+    countTap += 1;
+    console.log(countTap);
+    if(countTap == 1){
+        console.log("スタート");
+        catName.start();
+        catName.onresult = (event) => {
+            text = event.results[0][0].transcript;
+        };
+        btn.textContent='終了する';       
+    }else{
+        catName.stop();
+        btn.textContent='マイク';
+        countTap = 0;
+    };
+    if(text != ""){
+        content.value = text;
     }
+    console.log(countTap);
+},false);
 
-    btn.addEventListener('click',function(){
-        countTap += 1;
-        if(countTap == 1){
-            catName.start();
-            catName.onresult = (event) => {
-                text = event.results[0][0].transcript;
-            };
-            btn.textContent='終了する';       
-        }else{
-            catName.stop();
-            btn.textContent='マイク';
-            countTap = 0;
-        };
-        if(text != ""){
-            content.value = text;
-        }
-        console.log(countTap);
-    },false);
-
-    catName.addEventListener('result',function(e){
-        var text = e.results[0][0].transcript;     
-        if(text.length == 0){
-            content.value = "聞き取れませんでした";
-        }else{
-            content.value = text;
-        };
-        
-        console.log(content.value);
-     },false);
+catName.addEventListener('result',function(e){
+    var text = e.results[0][0].transcript;     
+    if(text.length == 0){
+        content.value = "聞き取れませんでした";
+    }else{
+        content.value = text;
+    };
+    
+    console.log(content.value);
 },false);
 
 
